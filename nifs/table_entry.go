21935726a85afd4c30aa09ef3d1f1b56b9d8c6da
Oskar Gjølga <os.gjolga@stud.uis.no>
2021-08-15 14:25:41 +0200

    aligned point count in table output

--- nifs/table_entry.go
+++ nifs/table_entry.go
@@ -1,6 +1,14 @@
 package nifs
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
+
+const (
+	TEAM_NAME_WIDTH = 20
+)
 
 type TableEntry struct {
 	TeamName      string
@@ -31,6 +39,10 @@ func (t *TableEntry) GoalDiff() int {
 	return t.Goals - t.GoalsAgainst
 }
 
+func (t *TableEntry) separator() string {
+	return strings.Repeat(" ", TEAM_NAME_WIDTH-utf8.RuneCountInString(t.TeamName))
+}
+
 func (t TableEntry) String() string {
-	return fmt.Sprintf("%s 		%d", t.TeamName, t.Points)
+	return fmt.Sprintf("%s%s%d", t.TeamName, t.separator(), t.Points)
 }
